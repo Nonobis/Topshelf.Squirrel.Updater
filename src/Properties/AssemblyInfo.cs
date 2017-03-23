0f5f17b4375e0214462e3689f17ee6650c8b0a94
Arnaud Dartois <nonobis@gmail.com>
2017-03-23 12:30:28 +0100

    Update AssemblyInfo

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -2,35 +2,30 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// Управление общими сведениями о сборке осуществляется с помощью 
-// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
-// связанные со сборкой.
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
 [assembly: AssemblyTitle("Topshelf.Squirrel.Windows")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Fork of https://github.com/Kibnet/Topshelf.Squirrel.Windows by Kibnet")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Kibnet")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Topshelf.Squirrel.Windows")]
-[assembly: AssemblyCopyright("Copyright © Kibnet 2016")]
+[assembly: AssemblyCopyright("Copyright © A. Dartois 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
-// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
-// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyFileVersion("0.0.1")]
 [assembly: ComVisible(false)]
-
-// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
 [assembly: Guid("aef86ad8-cece-426d-9dc7-6313b6046cda")]
 
-// Сведения о версии сборки состоят из следующих четырех значений:
-//
-//      Основной номер версии
-//      Дополнительный номер версии 
-//   Номер сборки
-//      Редакция
-//
-// Можно задать все значения или принять номера сборки и редакции по умолчанию 
-// используя "*", как показано ниже:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-//[assembly: AssemblyFileVersion("1.0.0.0")]
