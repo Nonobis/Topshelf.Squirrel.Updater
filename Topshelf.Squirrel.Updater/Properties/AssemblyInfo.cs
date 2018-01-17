a848fb4fd92ffbc9fb40b698c16313c59cce007b
Arnaud Dartois <nonobis@gmail.com>
2018-01-17 13:47:44 +0100

    - Increment version to 0.0.7 - Add Alpha tag to 0.0.7 version

--- Topshelf.Squirrel.Updater/Properties/AssemblyInfo.cs
+++ Topshelf.Squirrel.Updater/Properties/AssemblyInfo.cs
@@ -23,8 +23,9 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.6")]
-[assembly: AssemblyFileVersion("0.0.6")]
+[assembly: AssemblyVersion("0.0.7")]
+[assembly: AssemblyFileVersion("0.0.7")]
+[assembly: AssemblyInformationalVersion("0.0.7-alpha")]
 [assembly: ComVisible(false)]
 [assembly: Guid("aef86ad8-cece-426d-9dc7-6313b6046cda")]
 
